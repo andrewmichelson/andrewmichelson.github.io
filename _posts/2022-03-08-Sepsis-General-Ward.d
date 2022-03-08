34d01955ccfe1bd7c386efef86736d485f2f3c64
Michelson Lab <92810558+andrewmichelson@users.noreply.github.com>
2022-03-08 08:04:37 -0600

    Create 2022-03-08-Sepsis-General-Ward.d

---
+++ _posts/2022-03-08-Sepsis-General-Ward.d
@@ -0,0 +1,14 @@
+---
+title: "[publication] Sepsis Prediction for the General Ward Setting"
+date: 2022-03-08T00:00:00+00:00
+author: Andrew P. Michelson, MD
+layout: post
+permalink: /sepsis-prediction-general-ward/
+categories: publication
+tags: [EHR, publication, sepsis, AI/ML, predictive modeling]
+---
+
+#### Sepsis Prediction for the General Ward Setting
+Check out our latest work predicting sepsis for patients at the highest risk for delayed recognition!
+<br>
+https://www.frontiersin.org/articles/10.3389/fdgth.2022.848599/full
